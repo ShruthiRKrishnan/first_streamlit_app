2fda9efeb6d8f513e0a2a50909c080df3ead458c
Shruthi Ramakrishnan <118613371+ShruthiRKrishnan@users.noreply.github.com>
2022-11-19 19:36:28 +0530

    changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title("Hello,Shruthi!")
 
-streamlit.header("Breakfast")
-streamlit.text("1.Coffee")
-streamlit.text("2.tea")
-streamlit.text("3.Dosa")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
